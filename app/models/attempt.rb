25ab6f642444d8df0495022f6c441aa3c6913a55
Rachel Warbelow <rachelwarbelow@gmail.com>
2021-04-12 20:26:31 -0600

    Adds lesson for group by with multiple characteristics

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -2,29 +2,36 @@ class Attempt
   attr_reader :submission, :results, :message
   def initialize(input)
     @submission = format(input)
+    @results    = []
   end
 
   def check!
     temp_container = []
     temp_message   = ""
-    DB.transaction do 
-      begin
-        temp_container << DB.fetch(submission).to_a  
-      rescue Sequel::ForeignKeyConstraintViolation, Sequel::DatabaseError => e
-        temp_message += e.to_s
-      end
-      raise Sequel::Rollback  
+    unless unauthorized?
+        DB.transaction do 
+          begin
+            temp_container << DB.fetch(submission).to_a  
+          rescue Sequel::ForeignKeyConstraintViolation, Sequel::DatabaseError => e
+            temp_message += e.to_s
+          end
+          raise Sequel::Rollback  
+        end
+      @results = temp_container.flatten
+      @message = temp_message
     end
-    @results = temp_container.flatten
-    @message = temp_message
   end
 
   def message
-    if submission.downcase.include?("drop ") || submission.downcase.include?("truncate ")
+    if unauthorized?
       "Sorry, you do not have permission to do that."
     else
       @message
     end
+  end
+
+  def unauthorized?
+    submission.downcase.include?("drop ") || submission.downcase.include?("truncate ")
   end
 
   def format(input)
